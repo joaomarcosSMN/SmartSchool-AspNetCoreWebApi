d9eea45d5153f21baaf2d2ec293d3c76d1f85dc7
Joao Marcos <joao.marcos@smn.com.br>
2021-01-06 12:02:23 -0300

    SPRINT001-2020: migration e ajuste do AlunoController

--- SmatSchool.WebAPI/Controllers/AlunoController.cs
+++ SmatSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmatSchool.WebAPI.Data;
 using SmatSchool.WebAPI.Models;
 
 namespace SmatSchool.WebAPI.Controllers
@@ -10,58 +12,71 @@ namespace SmatSchool.WebAPI.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
-        public List<Aluno> Alunos = new List<Aluno>() {
-            new Aluno() {
-                Id = 1,
-                Nome = "Joao",
-                Sobrenome = "Marcos",
-                Telefone = "4968498489"
-            },
-            new Aluno() {
-                Id = 2,
-                Nome = "Neymar",
-                Sobrenome = "Jr",
-                Telefone = "489498494"
-            },
-            new Aluno() {
-                Id = 3,
-                Nome = "Zinedine",
-                Sobrenome = "Zidane",
-                Telefone = "119188118"
-            },
-        };
-        public AlunoController() { }
+    // private readonly DataContext context;
+    private readonly DataContext _context;
+
+    public AlunoController(DataContext context) {
+    //   this.context = context;
+        _context = context;
+    }
 
         [HttpGet]
         public IActionResult Get() {
-            return Ok("Alunos: Joao, Marcos");
+            return Ok(_context.Alunos);
         }
 
         [HttpGet("byId/{id}")]
         public IActionResult GetById(int id) {
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
             if (aluno == null) return BadRequest("Aluno não encontrado");
             
             return Ok(aluno);
         }
 
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => 
+                a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome)
+            );
+            if(aluno == null) return BadRequest("Aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
         [HttpPost]
         public IActionResult Post(Aluno aluno) {
+            _context.Add(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPut("{id}")]
         public IActionResult Put(int id, Aluno aluno) {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null) return BadRequest("Aluno não encontrado");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPatch("{id}")]
         public IActionResult Patch(int id, Aluno aluno) {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null) return BadRequest("Aluno não encontrado");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
         
         [HttpPut("{id}")]
         public IActionResult Delete(int id) {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if(aluno == null) return BadRequest("Aluno não encontrado");
+            _context.Remove(aluno);
+            _context.SaveChanges();
             return Ok();
         }
     }
