0511194b68ef573df5cdca323b48b36163723033
Joao Marcos <joao.marcos@smn.com.br>
2021-01-08 09:13:41 -0300

    SPRINT001-2021: implementando ProfessorDto

---
+++ SmatSchool.WebAPI/DTOs/ProfessorDto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SmatSchool.WebAPI.DTOs
+{
+    public class ProfessorDto
+    {
+        public int Id { get; set; }
+        public int Registro { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;    
+        public bool Ativo { get; set; } = true;
+    }
+}
