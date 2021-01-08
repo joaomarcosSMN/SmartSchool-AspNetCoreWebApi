3b56f4544f4db69fd58640000e93c1b462105192
Joao Marcos <joao.marcos@smn.com.br>
2021-01-08 10:39:21 -0300

    SPRINT001-2021: config inicial para swagger

--- SmatSchool.WebAPI/Startup.cs
+++ SmatSchool.WebAPI/Startup.cs
@@ -42,6 +42,17 @@ namespace SmatSchool.WebAPI
             // services.AddSingleton<IRepository, Repository>();
             // services.AddTransient<IRepository, Repository>();
             services.AddScoped<IRepository, Repository>();
+
+            services.AddSwaggerGen(options => 
+            {
+                options.SwaggerDoc(
+                    "smartschoolapi",
+                    new Microsoft.OpenApi.Models.OpenApiInfo() 
+                    {
+                        Title = "SmartSchool API",
+                        Version = "1.0"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +66,13 @@ namespace SmatSchool.WebAPI
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseSwagger()
+                .UseSwaggerUI(options => 
+                {
+                    options.SwaggerEndpoint("/swagger/smartschoolapi/swagger.json", "smartschoolapi");
+                    options.RoutePrefix = "";
+                });
 
             // app.UseAuthorization();
 
