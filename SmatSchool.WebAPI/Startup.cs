8ac4aca22c2cf5892cd3d613c8fb5894ec705993
Joao Marcos <joao.marcos@smn.com.br>
2021-01-08 08:40:09 -0300

    SPRINT001-2020: ajustes de packages e middlewares

--- SmatSchool.WebAPI/Startup.cs
+++ SmatSchool.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,15 +32,16 @@ namespace SmatSchool.WebAPI
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
 
-            // services.AddSingleton<IRepository, Repository>();
-            // services.AddTransient<IRepository, Repository>();
-            services.AddScoped<IRepository, Repository>();
-
-
             services.AddControllers()
                     .AddNewtonsoftJson(
                         option => option.SerializerSettings.ReferenceLoopHandling = 
                                 Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // services.AddSingleton<IRepository, Repository>();
+            // services.AddTransient<IRepository, Repository>();
+            services.AddScoped<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
